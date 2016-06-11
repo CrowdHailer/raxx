9f0c2c471dc93cd1452bc8cc0f2920913b322a39
CrowdHailer <peterhsaxton@gmail.com>
2016-06-11 09:57:24 +0100

    add hex publish information

--- mix.exs
+++ mix.exs
@@ -11,33 +11,35 @@ defmodule Raxx.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps,
+     description: description,
+     package: package]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger, :httpoison]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # To depend on another app inside the umbrella:
-  #
-  #   {:myapp, in_umbrella: true}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:cowboy, "1.0.4"},
       {:httpoison, "~> 0.8.0"}
     ]
+  end
+
+  defp description do
+    """
+    A Elixir webserver interface, for stateless HTTP.
+
+    Raxx exists to simplify handling the HTTP request-response cycle.
+    It deliberately does not handle other communication styles that are part of the modern web.
+    """
+  end
+
+  defp package do
+    [
+     maintainers: ["Peter Saxton"],
+     licenses: ["Apache 2.0"],
+     links: %{"GitHub" => "https://github.com/crowdhailer/raxx"}]
   end
 end

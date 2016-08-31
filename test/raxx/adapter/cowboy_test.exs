ac95aac4abaad47d7ed5def07789dc72c9297b6b
CrowdHailer <peterhsaxton@gmail.com>
2016-08-31 19:44:54 +0100

    nice tests curousy of james

--- test/raxx/adapter/cowboy_test.exs
+++ test/raxx/adapter/cowboy_test.exs
@@ -24,7 +24,7 @@ defmodule RedirectRequest do
     redirect("/pong")
   end
 
-  def handle_request(request, _env) do
+  def handle_request(_request, _env) do
     ok("pong")
   end
 end
@@ -33,33 +33,29 @@ defmodule Raxx.CowboyTest do
   use ExUnit.Case, async: true
 
   setup_all do
-    {:ok, agent} = Agent.start_link(fn -> 1 end)
     case Application.ensure_all_started(:cowboy) do
       {:ok, _} ->
-        {:ok, %{agent: agent}}
+        :ok
       {:error, {:cowboy, _}} ->
         raise "could not start the cowboy application. Please ensure it is listed " <>
               "as a dependency both in deps and application in your mix.exs"
     end
   end
 
-  setup %{agent: agent} do
-    id = Agent.get_and_update(agent, fn (i) -> {i, i + 1}end)
-    {:ok, %{port: 10_000 + id}}
-  end
-
-  def raxx_up(port, app \\ {Forwarder, self}) do
+  def raxx_up(%{case: case, test: test}, app \\ {Forwarder, self}) do
+    name = {case, test}
     routes = [
       {:_, Raxx.Adapters.Cowboy.Handler, app}
     ]
     dispatch = :cowboy_router.compile([{:_, routes}])
     env = [dispatch: dispatch]
-    :cowboy.start_http(
-      :"test_on_#{port}",
+    {:ok, _pid} = :cowboy.start_http(
+      name,
       2,
-      [port: port],
+      [port: 0],
       [env: env]
     )
+    :ranch.get_port(name)
   end
 
   @tag :skip
@@ -72,8 +68,9 @@ defmodule Raxx.CowboyTest do
     assert body == returned
   end
 
-  test "add custom headers to request", %{port: port} do
-    {:ok, _pid} = raxx_up(port, {FakeHeader, %{}})
+  @tag :now
+  test "add custom headers to request", context do
+    port = raxx_up(context, {FakeHeader, %{}})
     {:ok, %{headers: headers}} = HTTPoison.get("localhost:#{port}")
     header = Enum.find(headers, fn
       ({"custom-header", _}) -> true
@@ -82,30 +79,30 @@ defmodule Raxx.CowboyTest do
     assert {_, "my-value"} = header
   end
 
-  test "redirection", %{port: port} do
-    {:ok, _pid} = raxx_up(port, {RedirectRequest, %{}})
+  test "redirection", context do
+    port = raxx_up(context, {RedirectRequest, %{}})
     {:ok, %{body: body}} = HTTPoison.get("localhost:#{port}/ping", %{}, [follow_redirect: true])
     assert "pong" == body
   end
 
-  test "post some unknown binary content", %{port: port} do
-    {:ok, _pid} = raxx_up(port)
+  test "post some unknown binary content", context do
+    port = raxx_up(context)
     {:ok, _resp} = HTTPoison.post("localhost:#{port}", "blah blah", [{"content-type", "unknown/stuff"}])
     assert_receive %{headers: %{"content-type" => type}, body: body}
     assert "unknown/stuff" == type
     assert "blah blah" == body
   end
 
-  test "post simple form encoding", %{port: port} do
-    {:ok, _pid} = raxx_up(port)
+  test "post simple form encoding", context do
+    port = raxx_up(context)
     {:ok, _resp} = HTTPoison.post("localhost:#{port}", {:form, [{"foo", "bar"}]})
     assert_receive %{headers: %{"content-type" => type}, body: body}
     assert "application/x-www-form-urlencoded" <> _ = type
     assert %{"foo" => "bar"} == body
   end
 
-  test "post multipart form with file", %{port: port} do
-    {:ok, _pid} = raxx_up(port)
+  test "post multipart form with file", context do
+    port = raxx_up(context)
     body = {:multipart, [{"foo", "bar"}, {:file, "/etc/hosts"}]}
     {:ok, _resp} = HTTPoison.post("localhost:#{port}", body)
     assert_receive %{headers: %{"content-type" => type}, body: body}
@@ -114,8 +111,8 @@ defmodule Raxx.CowboyTest do
     assert file.contents
   end
 
-  test "assumed content type is html", %{port: port} do
-    {:ok, _pid} = raxx_up(port)
+  test "assumed content type is html", context do
+    port = raxx_up(context)
     {:ok, resp} = HTTPoison.get("localhost:#{port}")
     {"content-type", content_type} = Enum.find(resp.headers, fn
       ({"content-type", _}) -> true
@@ -124,8 +121,8 @@ defmodule Raxx.CowboyTest do
     assert "text/html" == content_type
   end
 
-  test "setup cowboy", %{port: port} do
-    {:ok, _pid} = raxx_up(port)
+  test "setup cowboy", context do
+    port = raxx_up(context)
     {:ok, _resp} = HTTPoison.get("localhost:#{port}")
     assert_receive %{host: "localhost"}
     {:ok, _resp} = HTTPoison.get("localhost:#{port}")

92efc116b877568a803e59a0ccd40debeda9b2ea
CrowdHailer <peterhsaxton@gmail.com>
2016-12-05 17:59:30 +0000

    add comments for next steps

--- lib/raxx/parsers/multipart.ex
+++ lib/raxx/parsers/multipart.ex
@@ -1,4 +1,20 @@
 defmodule Raxx.Parsers.Multipart do
+  @moduledoc """
+  This is all defined in the multipart/form-data [rfc7578](https://tools.ietf.org/html/rfc7578)
+
+  - Add an `encode/1` function that can be used to create a body,
+    mainly for testing purposes but also can be exposed as part of a 7578 hex package.
+  - Consider separate parsing of data to named list [{"field-name[]", "data"}] and grouping to a map via "field-name[]"
+  - this is an interesting library that might want to be emulated for nested queries. https://github.com/spiceworks/httpoison-form-data/blob/master/lib/form_data.ex
+  - plug adapter test for failed decodings
+
+  - Have uploads look as much like a file as possible
+  http://ruby-doc.org/stdlib-1.9.3/libdoc/tempfile/rdoc/Tempfile.html#method-i-path
+  - Read about erlang [io protocol](http://erlang.org/doc/apps/stdlib/io_protocol.html) to this end
+  - Comparison listing the concerns of an image uploader
+  https://infinum.co/the-capsized-eight/best-rails-image-uploader-paperclip-carrierwave-refile
+  """
+  # TODO add error case
   def parse(request) do
     case Raxx.Request.content_type(request) do
       {"multipart/form-data", "boundary=" <> boundary} ->

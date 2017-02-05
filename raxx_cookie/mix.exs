dfd430471762e994fb798ccdadee403c14acefa9
CrowdHailer <peterhsaxton@gmail.com>
2017-02-05 15:34:20 +0000

    publish raxx cookie

--- raxx_cookie/mix.exs
+++ raxx_cookie/mix.exs
@@ -7,7 +7,9 @@ defmodule Raxx.Cookie.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     description: description(),
+     package: package()]
   end
 
   def application do
@@ -15,6 +17,21 @@ defmodule Raxx.Cookie.Mixfile do
   end
 
   defp deps do
-    []
+    [
+      {:ex_doc, ">= 0.0.0", only: :dev}
+    ]
+  end
+
+  defp description do
+    """
+    Manage cookies in Raxx applications.
+    """
+  end
+
+  defp package do
+    [
+     maintainers: ["Peter Saxton"],
+     licenses: ["Apache 2.0"],
+     links: %{"GitHub" => "https://github.com/CrowdHailer/raxx/tree/master/raxx_cookie"}]
   end
 end

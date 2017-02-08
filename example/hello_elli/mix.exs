8948d736d3ef05cb288b5f23ecc6ac30119ad744
CrowdHailer <peterhsaxton@gmail.com>
2017-02-08 19:51:45 +0000

    update elli example

--- example/hello_elli/mix.exs
+++ example/hello_elli/mix.exs
@@ -10,27 +10,15 @@ defmodule HelloElli.Mixfile do
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger],
      mod: {HelloElli, []}]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:elli, "~> 1.0"},
-      {:raxx, ">= 0.0.0", path: "../../"}
+      {:raxx_elli, ">= 0.0.0", path: "../../raxx_elli"},
     ]
   end
 end

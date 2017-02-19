fec48b90492c83287c641e466179bc95b4853fea
CrowdHailer <peterhsaxton@gmail.com>
2017-02-19 17:02:23 +0000

    release tranfer-encoding and connection header helpers

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Raxx.Mixfile do
 
   def project do
     [app: :raxx,
-     version: "0.10.4",
+     version: "0.10.5",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,

9c9ae46cab7dc333f9c468599c6ac4fbc3d257f9
CrowdHailer <peterhsaxton@gmail.com>
2017-02-26 07:31:12 +0000

    upgrade raxx_cowboy release include ssl support

--- raxx_cowboy/mix.exs
+++ raxx_cowboy/mix.exs
@@ -3,7 +3,7 @@ defmodule Raxx.Cowboy.Mixfile do
 
   def project do
     [app: :raxx_cowboy,
-     version: "0.1.1",
+     version: "0.2.0",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,

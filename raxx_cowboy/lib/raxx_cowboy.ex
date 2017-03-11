fc51654df28ccde6c5204ba3c92099141559e3e8
CrowdHailer <peterhsaxton@gmail.com>
2017-03-11 07:51:53 +0000

    directly start ranch supervisor

--- raxx_cowboy/lib/raxx_cowboy.ex
+++ raxx_cowboy/lib/raxx_cowboy.ex
@@ -1,2 +1,23 @@
 defmodule Raxx.Cowboy do
+  def start_link(raxx_app, server_options) do
+    dispatch = dispatch_for(raxx_app)
+    name = Keyword.get(server_options, :name, __MODULE__)
+    acceptors = Keyword.get(server_options, :acceptors, 100)
+    port = Keyword.get(server_options, :port)
+    :ranch_listener_sup.start_link(
+      name,
+      acceptors,
+      :ranch_tcp,
+      [port: port],
+      :cowboy_protocol,
+      [env: [dispatch: dispatch]]
+    )
+  end
+
+  defp dispatch_for({handler, config}) do
+    routes = [
+      {:_, Raxx.Cowboy.Handler, {handler, config}}
+    ]
+    dispatch = :cowboy_router.compile([{:_, routes}])
+  end
 end
--- raxx_cowboy/test/raxx/cowboy/response_test.exs
+++ raxx_cowboy/test/raxx/cowboy/response_test.exs
@@ -1,22 +1,11 @@
 defmodule Raxx.Cowboy.ResponseTest do
   use Raxx.Verify.ResponseCase
 
-  case Application.ensure_all_started(:cowboy) do
-    {:ok, _} ->
-      :ok
-    {:error, {:cowboy, _}} ->
-      raise "could not start the cowboy application. Please ensure it is listed " <>
-            "as a dependency both in deps and application in your mix.exs"
-  end
-
   setup %{case: case, test: test} do
     name = {case, test}
-    routes = [
-      {:_, Raxx.Cowboy.Handler, {__MODULE__, %{target: self()}}}
-    ]
-    dispatch = :cowboy_router.compile([{:_, routes}])
-    env = [dispatch: dispatch]
-    {:ok, _pid} = :cowboy.start_http(name, 2, [port: 0], [env: env])
+    raxx_app = {__MODULE__, %{target: self()}}
+
+    Raxx.Cowboy.start_link(raxx_app, port: 0, name: name, acceptors: 2)
     {:ok, %{port: :ranch.get_port(name)}}
   end
 

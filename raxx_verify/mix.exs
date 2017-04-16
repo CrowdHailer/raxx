7369c7d1b128a32fe6ed2b7ae89fe0e69a12bcf8
CrowdHailer <peterhsaxton@gmail.com>
2017-04-16 15:51:24 +0100

    publish raxx_verify

--- raxx_verify/mix.exs
+++ raxx_verify/mix.exs
@@ -4,20 +4,35 @@ defmodule Raxx.Verify.Mixfile do
   def project do
     [app: :raxx_verify,
      version: "0.1.0",
-     elixir: "~> 1.3",
+     elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     description: description(),
+     package: package()]
   end
 
   def application do
-    [applications: [:logger]]
+    [extra_applications: [:logger]]
   end
 
   defp deps do
     [
-      {:httpoison, "~> 0.8.0"},
-      {:raxx_server_sent_events, ">= 0.0.0", path: "../raxx_server_sent_events"}
+      {:httpoison, "~> 0.8"},
+      {:ex_doc, ">= 0.0.0", only: :dev}
     ]
+  end
+
+  defp description do
+    """
+    Cowboy adapter for the Raxx webserver interface
+    """
+  end
+
+  defp package do
+    [
+     maintainers: ["Peter Saxton"],
+     licenses: ["Apache 2.0"],
+     links: %{"GitHub" => "https://github.com/CrowdHailer/raxx/tree/master/raxx_cowboy"}]
   end
 end

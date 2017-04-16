8d8de8a1939e5c9a6b3c776a9968a149c2660af3
CrowdHailer <peterhsaxton@gmail.com>
2017-04-16 13:09:09 +0100

    use elixir 1.4 and auto inlcude dependent applications

--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule Raxx.Mixfile do
 
   def project do
     [app: :raxx,
-     version: "0.10.5",
-     elixir: "~> 1.3",
+     version: "0.11.0",
+     elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
@@ -14,13 +14,13 @@ defmodule Raxx.Mixfile do
   end
 
   def application do
-    [applications: [:logger, :mime]]
+    [extra_applications: [:logger]]
   end
 
   defp deps do
     [
       {:mime, "~> 1.0"},
-      {:plug, "~> 1.2.2"}, # DEBT remove; currently used for query strings etc
+      {:plug, "~> 1.2"}, # DEBT remove; currently used for query strings etc
       {:http_status, "~> 0.2"},
       {:ex_doc, ">= 0.0.0", only: :dev},
     ]

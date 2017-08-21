366d65ae4e2e4040765d19ea9e3f92cac1ffd948
CrowdHailer <peterhsaxton@gmail.com>
2017-08-21 20:02:42 +0100

    remove example dir

--- example/cowboy_example/test/cowboy_example_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule CowboyExampleTest do
-  use ExUnit.Case
-  doctest CowboyExample
-
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
-end
--- example/hello_elli/test/hello_elli_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule HelloElliTest do
-  use ExUnit.Case
-  doctest HelloElli
-
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
-end

1799fc3e18b375f850e2002837abf52583037494
CrowdHailer <peterhsaxton@gmail.com>
2017-09-22 05:22:32 +0100

    remove verify experiment

--- raxx_verify/lib/raxx/verify/forwarder.ex
+++
@@ -1,7 +0,0 @@
-defmodule Raxx.Verify.Forwarder do
-  def handle_request(request, env) do
-    pid = Map.get(env, :target)
-    send(pid, request)
-    Raxx.Response.no_content()
-  end
-end
--- raxx_verify/test/raxx_verify_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule Raxx.VerifyTest do
-  use ExUnit.Case
-  doctest Raxx.Verify
-
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
-end

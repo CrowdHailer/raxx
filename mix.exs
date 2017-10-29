02ee59d9731f5ae6a56a80e070fecdd8726ea231
CrowdHailer <peterhsaxton@gmail.com>
2017-10-29 15:27:15 +0000

    second release candidate for 1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Raxx.Mixfile do
   def project do
     [
       app: :raxx,
-      version: "0.14.0",
+      version: "1.0.0-rc.1",
       elixir: "~> 1.5",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,

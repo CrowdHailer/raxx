20cdf61c6cdbc7a297a66a5a372e1cd6c94fb783
Charles <CharlesOkwuagwu@users.noreply.github.com>
2017-11-10 09:30:46 +0100

    Case-Insensitive and duplicate headers are valid.
    I think the responsibility of having duplicate headers should be left to the user.
    https://tools.ietf.org/html/rfc7230#section-3.2.2 suggests that in some cases it is allowed.
    
    Also, as per rfc, case-insensitive headers are valid

--- lib/raxx.ex
+++ lib/raxx.ex
@@ -255,14 +255,6 @@ defmodule Raxx do
       [{"referer", "example.com"}, {"accept", "text/html"}]
   """
   def set_header(message = %{headers: headers}, name, value) do
-    if String.downcase(name) != name do
-      raise "Header keys must be lowercase"
-    end
-
-    if :proplists.is_defined(name, headers) do
-      raise "Headers should not be duplicated"
-    end
-
     %{message | headers: headers ++ [{name, value}]}
   end
 

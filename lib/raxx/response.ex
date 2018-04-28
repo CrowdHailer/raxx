2ba905152f75023b29a673e36c17b7d3ddcadf10
CrowdHailer <peterhsaxton@gmail.com>
2018-04-28 14:58:33 +0100

    remove commented out response helpers

--- lib/raxx/response.ex
+++ lib/raxx/response.ex
@@ -26,40 +26,4 @@ defmodule Raxx.Response do
 
   @enforce_keys [:status, :headers, :body]
   defstruct @enforce_keys
-
-  # DEBT I never used these, will they be missed?
-  # @doc """
-  # The response is marked as an interim response.
-  #
-  # https://tools.ietf.org/html/rfc7231#section-6.2
-  # """
-  # def informational?(%{status: code}), do: 100 <= code and code < 200
-  #
-  # @doc """
-  # The response indicates that client request was received, understood, and accepted.
-  #
-  # https://tools.ietf.org/html/rfc7231#section-6.3
-  # """
-  # def success?(%{status: code}), do: 200 <= code and code < 300
-  #
-  # @doc """
-  # The response indicates that further action needs to be taken by the client.
-  #
-  # https://tools.ietf.org/html/rfc7231#section-6.4
-  # """
-  # def redirect?(%{status: code}), do: 300 <= code and code < 400
-  #
-  # @doc """
-  # The response indicates that the client sent an incorrect request.
-  #
-  # https://tools.ietf.org/html/rfc7231#section-6.5
-  # """
-  # def client_error?(%{status: code}), do: 400 <= code and code < 500
-  #
-  # @doc """
-  # The response indicates that the server is incapable of acting upon the request.
-  #
-  # https://tools.ietf.org/html/rfc7231#section-6.6
-  # """
-  # def server_error?(%{status: code}), do: 500 <= code and code < 600
 end

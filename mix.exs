59fb3b42108991a7bc24638a9885f4863d8997c0
CrowdHailer <peterhsaxton@gmail.com>
2018-08-18 19:21:24 +0100

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Raxx.Mixfile do
   def project do
     [
       app: :raxx,
-      version: "0.15.6",
+      version: "0.15.7",
       elixir: "~> 1.6",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,

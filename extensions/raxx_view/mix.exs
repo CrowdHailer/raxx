433f4ff482533c2b603cbb41c6fb8e6d65d1410f
CrowdHailer <peterhsaxton@gmail.com>
2019-02-06 20:15:04 +0000

    minor release of raxx_view

--- extensions/raxx_view/mix.exs
+++ extensions/raxx_view/mix.exs
@@ -4,7 +4,7 @@ defmodule RaxxView.MixProject do
   def project do
     [
       app: :raxx_view,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
